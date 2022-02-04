a40c65461c1f5dff7256ae5446dac0d750b631fc
Lana Souza <laniziamunique@gmail.com>
2022-02-04 18:00:54 -0300

    pagina da home atualizada

--- src/home/index.js
+++ src/home/index.js
@@ -1,17 +1,50 @@
+// Parte de js para fazer o menu hamburger
 
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
+(function () {
+  var navButton = document.querySelector('#nav-menu-button');
+  var navUl = document.querySelector('.nav-ul');
 
-hamburger.addEventListener('click', () => {
-    toggleClasses();
-});
+  function toggleMobileMenu() {
+    navUl.classList.toggle('hide-ul');
+  }
+
+  navButton.onclick = toggleMobileMenu;
+})();
 
 
-function toggleClasses() {
-	hamburger.classList.toggle('open');
-    menu.classList.toggle('open');
+// Aqui é a parte que configura os slides
+
+var slideIndex = 1;
+showSlides(slideIndex); 
+
+// controle para ir para frente e para trás nas imagens
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
 
-setTimeout(toggleClasses, 500);
-setTimeout(toggleClasses, 3000);
+// para controlar as imagens menores
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
+// aqui é a função que controla os slides
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+  var dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
